56eaa64b8e0a4d8cce26e037d2131fe1fe64ccdb
HXHdevs <jordisonnicolas@gmail.com>
2024-08-28 00:00:35 -0600

    Creando DB y usandola

---
+++ SQL-MySQL/QSL-queries.sql
@@ -0,0 +1,7 @@
+-- Muestra las bases de datos
+SHOW DATABASES;
+-- Con el siguiente creamos una base de datos
+CREATE DATABASE appsalon;
+-- Podemos volver a tipear show databases para mostrar las dbs existentes y debe mostrar appsalon
+-- despues aplicamos USE seguido de database para pocisioanrnos sobre la DB
+USE appsalon
