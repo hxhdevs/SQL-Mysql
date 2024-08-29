2b6d26321455ada6145ca4162bc409012d1725dd
HXHdevs <jordisonnicolas@gmail.com>
2024-08-28 23:32:42 -0600

    Eliminando tablas e insertando datos

--- SQL-MySQL/QSL-queries.sql
+++ SQL-MySQL/QSL-queries.sql
@@ -274,3 +274,69 @@ MariaDB [appsalon]> SELECT * FROM servicios;
 |  5 | Peinado               | 100.00 |
 +----+-----------------------+--------+
 4 rows in set (0.000 sec)
+-- Eliminando tablas
+MariaDB [appsalon]> DROP TABLE servicios;
+
+Query OK, 0 rows affected (0.008 sec)
+MariaDB [appsalon]> CREATE TABLE servicios(
+    -> id INT NOT NULL AUTO_INCREMENT,
+    -> nombre VARCHAR(60) not null,
+    -> precio DECIMAL(6,2)not null,
+    -> PRIMARY KEY(id)
+    -> );
+Query OK, 0 rows affected (0.011 sec)
+
+MariaDB [appsalon]> CREATE TABLE reservaciones(
+    -> id INT(11) NOT NULL AUTO_INCREMENT,
+    -> nombre varchar(60) not null,
+    -> apellido varchar(60) not null,
+    -> hora time default null,
+    -> fecha date default null,
+    -> servicios varchar(255)not null,
+    -> primary key (id)
+    -> );
+Query OK, 0 rows affected (0.011 sec)
+	
+	INSERT INTO reservaciones (nombre, apellido, hora, fecha, servicios) VALUES
+        ('Juan', 'De la torre', '10:30:00', '2021-06-28', 'Corte de Cabello Adulto, Corte de Barba' ),
+        ('Antonio', 'Hernandez', '14:00:00', '2021-07-30', 'Corte de Cabello Niño'),
+        ('Pedro', 'Juarez', '20:00:00', '2021-06-25', 'Corte de Cabello Adulto'),
+        ('Mireya', 'Perez', '19:00:00', '2021-06-25', 'Peinado Mujer'),
+        ('Jose', 'Castillo', '14:00:00', '2021-07-30', 'Peinado Hombre'),
+        ('Maria', 'Diaz', '14:30:00', '2021-06-25', 'Tinte'),
+        ('Clara', 'Duran', '10:00:00', '2021-07-01', 'Uñas, Tinte, Corte de Cabello Mujer'),
+        ('Miriam', 'Ibañez', '09:00:00', '2021-07-01', 'Tinte'),
+        ('Samuel', 'Reyes', '10:00:00', '2021-07-02', 'Tratamiento Capilar'),
+        ('Joaquin', 'Muñoz', '19:00:00', '2021-06-28', 'Tratamiento Capilar'),
+        ('Julia', 'Lopez', '08:00:00', '2021-06-25', 'Tinte'),
+        ('Carmen', 'Ruiz', '20:00:00', '2021-07-01', 'Uñas'),
+        ('Isaac', 'Sala', '09:00:00', '2021-07-30', 'Corte de Cabello Adulto'),
+        ('Ana', 'Preciado', '14:30:00', '2021-06-28', 'Corte de Cabello Mujer'),
+        ('Sergio', 'Iglesias', '10:00:00', '2021-07-02', 'Corte de Cabello Adulto'),
+        ('Aina', 'Acosta', '14:00:00', '2021-07-30', 'Uñas'),
+        ('Carlos', 'Ortiz', '20:00:00', '2021-06-25', 'Corte de Cabello Niño'),
+        ('Roberto', 'Serrano', '10:00:00', '2021-07-30', 'Corte de Cabello Niño'),
+        ('Carlota', 'Perez', '14:00:00', '2021-07-01', 'Uñas'),
+        ('Ana Maria', 'Igleias', '14:00:00', '2021-07-02', 'Uñas, Tinte'),
+        ('Jaime', 'Jimenez', '14:00:00', '2021-07-01', 'Corte de Cabello Niño'),
+        ('Roberto', 'Torres', '10:00:00', '2021-07-02', 'Corte de Cabello Adulto'),
+        ('Juan', 'Cano', '09:00:00', '2021-07-02', 'Corte de Cabello Niño'),
+        ('Santiago', 'Hernandez', '19:00:00', '2021-06-28', 'Corte de Cabello Niño'),
+        ('Berta', 'Gomez', '09:00:00', '2021-07-01', 'Uñas'),
+        ('Miriam', 'Dominguez', '19:00:00', '2021-06-28', 'Corte de Cabello Niño'),
+        ('Antonio', 'Castro', '14:30:00', '2021-07-02', 'Corte de Cabello Adulti'),
+        ('Hugo', 'Alonso', '09:00:00', '2021-06-28', 'Corte de Barba'),
+        ('Victoria', 'Perez', '10:00:00', '2021-07-02', 'Uñas, Tinte'),
+        ('Jimena', 'Leon', '10:30:00', '2021-07-30', 'Uñas, Corte de Cabello Mujer'),
+        ('Raquel' ,'Peña', '20:30:00', '2021-06-25', 'Corte de Cabello Mujer');
+        
+  INSERT INTO servicios ( nombre, precio ) VALUES
+        ('Corte de Cabello Niño', 60),
+        ('Corte de Cabello Hombre', 80),
+        ('Corte de Barba', 60),
+        ('Peinado Mujer', 80),
+        ('Peinado Hombre', 60),
+        ('Tinte',300),
+        ('Uñas', 400),
+        ('Lavado de Cabello', 50),
+        ('Tratamiento Capilar', 150);
